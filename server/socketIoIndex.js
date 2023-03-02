d4dc29544d756ff4ccb7143df9ee43644030c98e
Gabe <gbennettbrandt@gmail.com>
2023-03-02 11:23:20 -0600

    morgan update

--- server/socketIoIndex.js
+++ server/socketIoIndex.js
@@ -17,7 +17,7 @@ const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
 
-app.use(morgan);
+app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
