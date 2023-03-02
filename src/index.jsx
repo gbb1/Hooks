76f0d5c15a1264e92324311b16c481b687edaacf
Gabe <gbennettbrandt@gmail.com>
2023-03-02 10:09:06 -0600

    Saving socket update

--- src/index.jsx
+++ src/index.jsx
@@ -25,7 +25,8 @@ import Logo from './components/elements/logo.jsx';
 // import FEScores from './components/FEScores.jsx';
 
 // CONNECT TO THE SOCKET AND SETUP CONTEXT ACCESS FOR CHILD COMPONENTS
-const socket = io('http://localhost:8089');
+// const socket = io('http://localhost:8089');
+const socket = io('http://3.16.90.3:8089');
 export const SocketContext = createContext(socket);
 
 const app = createRoot(document.getElementById('app'));
